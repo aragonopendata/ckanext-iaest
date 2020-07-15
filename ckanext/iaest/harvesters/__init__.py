6c7292840524ef49a44226a136f4bd28de24872b
galcober <galcober@deloitte.es>
2020-07-15 09:59:14 +0200

    ETL new branch

--- ckanext/iaest/harvesters/__init__.py
+++
@@ -1,4 +0,0 @@
-from ckanext.iaest.harvesters.rdf import IAESTRDFHarvester
-
-from ckanext.iaest.harvesters.xml import IAESTXMLHarvester
-from ckanext.iaest.harvesters._json import IAESTJSONHarvester
